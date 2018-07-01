b94f4ef4dd438967bd983d944849bb9637592c1f
Preetam <android.preetam@gmail.com>
2018-07-01 18:51:40 +0530

    tasks with date

--- api/static/js/main.js
+++ api/static/js/main.js
@@ -2,15 +2,16 @@ $(function () {
     // CHECKBOX CLICK LINE THROUGH
     $('.checkBoxGroup').on('click', function (e) {
         var checkBox = e.target
-        var divText = $(checkBox).closest('div.row').find('div.col-sm-10')
+        var divText = $(checkBox).closest('div.row').find('div.col-sm-6')
+        var divDate = $(checkBox).closest('div.row').find('div.col-sm-5')
         if ($(checkBox).is(':checked')) {
             divText.css('text-decoration', 'line-through')
+            divDate.css('text-decoration', 'line-through')
         } else {
             divText.css('text-decoration','none')
+            divDate.css('text-decoration','none')
         }
     });
-
-    
 
 
 });
