8531bcdcf46750aa792641f6190f8d35988d5872
Preetam <android.preetam@gmail.com>
2018-07-02 00:05:40 +0530

    single click, restore, permanent button working and clean code main js

--- api/static/js/main.js
+++ api/static/js/main.js
@@ -1,5 +1,4 @@
-$(function () {
-    // CHECKBOX CLICK LINE THROUGH
+function checkboxClickListener() {
     $('.checkBoxGroup').on('click', function (e) {
         var checkBox = e.target
         var divText = $(checkBox).closest('div.row').find('div.col-sm-6')
@@ -25,11 +24,16 @@ $(function () {
             dataType: 'json',
             complete: function (data) {
                 console.log(data);
+            },
+            error: function (xhr) {
+                console.log(xhr)
             }
         });
     });
+}
 
 
+function deleteButtonListener() {
     $('.deleteGroup').on('click', function (e) {
         var deleteIcon = e.target
         var id = $(deleteIcon).closest('div').data('id')
@@ -49,42 +53,95 @@ $(function () {
             complete: function (data) {
                 console.log(data);
                 $(li).remove()
+            },
+            error: function (xhr) {
+                console.log(xhr)
             }
         });
 
+    });
+}
 
-        // $.ajax({
-        //     type: 'DELETE',
-        //     url: '/api/v1/task/' + id + "/",
-        //     complete: function (data) {
-        //         console.log(data);
-        //         $(li).remove()
-        //     }
-        // });
+function restoreButtonClickListener() {
+    $('.restoreGroup').on('click', function (e) {
+        var deleteIcon = e.target
+        var li = $(deleteIcon).closest('li')
+        
+        
+        var divDelete = $(deleteIcon).closest('div.row').find('div.permanentDeleteGroup')
+        var id = $(divDelete).data('id')
+        console.log(id);
+        // use this when server side delete
+        $.ajax({
+            type: 'PATCH',
+            contentType: "application/json",
+            url: '/api/v1/task/' + id + "/",
+            data: JSON.stringify({
+                "is_deleted": false
+            }),
+            dataType: 'json',
+            complete: function (data) {
+                console.log(data);
+                $(li).remove()
+            },
+            error: function (xhr) {
+                console.log(xhr)
+            }
+        });
+
+    });
+}
+
+
+function permanentDeleteButtonClickListener() {
+    $('.permanentDeleteGroup').on('click', function (e) {
+        var deleteIcon = e.target
+        var id = $(deleteIcon).closest('div').data('id')
+        console.log(id);
+        var li = $(deleteIcon).closest('li')
+
+
+        // use this when server side delete
+        $.ajax({
+            type: 'DELETE',
+            contentType: "application/json",
+            url: '/api/v1/task/' + id + "/",
+            success: function (data) {
+                console.log(data);
+                $(li).remove()
+            },
+            error: function (xhr) {
+                console.log(xhr)
+            }
+        });
 
     });
 
+}
 
-    $('#trash').on('click', function (e) {
-        // /api/v1/task/?is_deleted=true
-        console.log('clicked');
 
+function homeClickListener() {
+
+    $('#home').one('click', function (e) {
 
         $.ajax({
             url: "/api/v1/task/",
             type: "get", //send it through get method
             data: {
-                'is_deleted': true
+                'is_deleted': false
             },
             success: function (response) {
-                //Do Something
-                // console.log(response);
+                trashClickListener()
+                $('#heading').text('Tasks')
                 data = response['objects']
                 content = $('#content').empty()
                 for (var i = 0; i < data.length; i++) {
                     $(content).append('<li class="list-group-item"> <div class = "row">\
-                                            <div class = "col-sm-1" >\
-                                            <i class="fas fa-undo"></i> </div>' +
+                    <div class="col-sm-1"> <div class="round">' +
+                        (data[i]['is_task_completed'] ? '<input type="checkbox" id="checkbox' + i + '" checked class="checkBoxGroup" />' :
+                            '<input type="checkbox" id="checkbox' + i + '" class="checkBoxGroup" />') +
+                        '<label for="checkbox' + i + '"></label> \
+                    </div> </div>' +
 
                         (data[i]['is_task_completed'] ? '<div class="col-sm-6 pT4" style="text-decoration: line-through;">'
 
@@ -109,16 +166,89 @@ $(function () {
 
 
                 }
+                checkboxClickListener()
+                deleteButtonListener()
 
 
             },
             error: function (xhr) {
                 //Do Something to handle error
                 console.log(xhr);
+                homeClickListener()
+            }
+        });
+
+    });
+
+
+
+}
+
+
+function trashClickListener() {
+
+    $('#trash').one('click', function (e) {
+        $.ajax({
+            url: "/api/v1/task/",
+            type: "get", //send it through get method
+            data: {
+                'is_deleted': true
+            },
+            success: function (response) {
+                homeClickListener()
+                var heading = $('#heading')
+                heading.text('Deleted')
+                heading.append('<p style="color:red;">Automatically deleted after 30 days</p>')
+                data = response['objects']
+                content = $('#content').empty()
+                for (var i = 0; i < data.length; i++) {
+                    $(content).append('<li class="list-group-item"> <div class = "row">\
+                                            <div class = "col-sm-1 restoreGroup" >\
+                                            <i class="fas fa-undo"></i> </div>' +
+
+                        (data[i]['is_task_completed'] ? '<div class="col-sm-6 pT4" style="text-decoration: line-through;">'
+
+                            :
+                            '<div class="col-sm-6 pT4">')
+
+                        +
+                        (data[i]['has_sub_tasks'] ? '<a href="#">' :
+                            '') +
+                        data[i]['title'] +
+                        (data[i]['has_sub_tasks'] ? '</a>' : '') +
+                        '</div>' +
+                        (data[i]['is_task_completed'] ? '<div class="col-sm-4" style="text-decoration: line-through">' :
+                            '<div class="col-sm-4">')
+
+                        +
+                        data[i]['due_date'] +
+                        '</div> <div class="col-sm-1 permanentDeleteGroup"\
+                                                    data-id = "' +
+                        data[i]['id'] +
+                        '" ><i class = "fa fa-trash" aria-hidden = "true" > </i> </div> </div> </li>')
+
+
+                }
+
+                restoreButtonClickListener()
+                permanentDeleteButtonClickListener()
+
+
+            },
+            error: function (xhr) {
+                //Do Something to handle error
+                console.log(xhr);
+                trashClickListener()
 
             }
         });
     });
 
 
+}
+
+$(function () {
+    checkboxClickListener()
+    deleteButtonListener()
+    trashClickListener()
 });
